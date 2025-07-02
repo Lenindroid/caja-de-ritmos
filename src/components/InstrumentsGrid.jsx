6f55ea36de0eb764dca0d8488c361076826e7a62
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-07-01 22:17:06 -0600

    dog bark fixed

--- src/components/InstrumentsGrid.jsx
+++ src/components/InstrumentsGrid.jsx
@@ -9,7 +9,7 @@ function InstrumentsGrid({setInstrument, currentVolume, power}) {
       <Instrument id="heater-1" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" description="Heater 1" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>E</Instrument>
       <Instrument id="heater-2" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3" description="Heater 2" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>A</Instrument>
       <Instrument id="heater-3" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3" description="Heater 3" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>S</Instrument>
-      <Instrument id="dog-bark" src="/sounds/dog-bark.mp3" description="Dog bark" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>D</Instrument>
+      <Instrument id="dog-bark" src="sounds/dog-bark.mp3" description="Dog bark" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>D</Instrument>
       <Instrument id="open-hh" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3" description="Open HH" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>Z</Instrument>
       <Instrument id="clap" src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3" description="Clap" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>X</Instrument>
       <Instrument id="cat-meow" src="sounds/meow.mp3" description="Cat meow" setInstrument={setInstrument} currentVolume={currentVolume} power={power}>C</Instrument>
